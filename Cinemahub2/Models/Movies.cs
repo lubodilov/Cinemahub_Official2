0820bec6a344b2735135e86018f011ff0fcd7674
ouplex <momchil26@gmail.com>
2022-03-21 20:54:57 +0200

    Movie model and DTO

---
+++ Cinemahub2/Models/Movies.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cinemahub2.Models
+{
+    public class Movies
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Genre { get; set; }
+        public string Director { get; set; }
+        public DateTime Released { get; set; }
+        public int Duration { get; set; }
+        public int ActorsId { get; set; }
+        //public Actor Actor { get; set; }
+        public int UserId { get; set; }
+        public string Author { get; set; }
+        public string AuthorEmail { get; set; }
+       
+       /* public Movies(int id, string name, Actor Actor, int userId, string author, string authorEmail, DateTime released, int duration)
+        {
+            Id = id;
+            Name = name;
+            Actor = Actor;
+            UserId = userId;
+            Author = author;
+            AuthorEmail = authorEmail;
+            Released = released;
+            Duration = duration;
+        }*/
+        public Movies()
+        {
+        }
+    }
+}
