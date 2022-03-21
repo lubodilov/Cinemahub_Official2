6be5f5547393507992e8f9b4b7a2c68adc93e4a5
lubod <lubo.daniel.dilov@gmail.com>
2022-03-21 23:06:22 +0200

    Added UserService

---
+++ Cinemahub2/Services/IUserService.cs
@@ -0,0 +1,17 @@
+﻿using Cinemahub2.Models;
+using Cinemahub2.Models.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cinemahub2.Services
+{
+    public interface IUserService
+    {
+        User GetEntityById(int id);
+        UserDTO GetById(int id);
+        void Update(int id, UserDTO userDTO);
+        void Delete(int id);
+    }
+}
